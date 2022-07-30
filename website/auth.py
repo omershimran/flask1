845411331c27c0eacc7b935918603689bef1ad05
omer <omershimran@gmail.com>
2022-07-30 13:58:08 +0300

    test

---
+++ website/auth.py
@@ -0,0 +1,70 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import  User
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, login_required, current_user
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("logged in!!", category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('wrong details!!!', category='error')
+        else:
+            flash('no user found', category='error')
+
+    return render_template("login.html", text="testing", user=current_user)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+
+@auth.route('/sign-up', methods=['POST', 'GET'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstname = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        print(firstname)
+        print(password1)
+        print(password2)
+
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            flash('user already exist!!!!',category='error')
+        elif len(email) < 3:
+            flash("email is too short", category="error")
+        elif len(firstname) < 2:
+            flash("name is too short", category="error")
+        elif len(password1) < 4:
+            flash("password is too short", category="error")
+        elif password2 != password1:
+            flash("password does not match", category="error")
+        else:
+            new_user = User(first_name=firstname, email=email, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash("added user")
+            return redirect(url_for('views.home'))
+
+    return render_template("sign_up.html", text="testing", user=current_user)
+
---
+++ website/views.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
+
+views = Blueprint('views', __name__)
+
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+    if request.method == 'POST':
+        data = request.form.get('data')
+        new_note = Note(data=data, user_id=current_user.id)
+        db.session.add(new_note)
+        db.session.commit()
+    return render_template("home.html", text="testing", user=current_user)
+
+
+@views.route('/delete-note', methods=[ 'POST'])
+def delete_note():
+    note = json.loads(request.data)
+    note_id = note['noteId']
+    note = Note.query.get(note_id)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+        else:
+            flash('this not is not your to delete...', category='error')
+
+    return jsonify({})
